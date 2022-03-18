9380b96bdac0039fd721df352289f34fb53772b0
Olexandr <super.lukianchuk2010@gmail.com>
2022-03-18 22:43:48 +0200

    created NoteItem.kt && OrderSection.kt && some refactoring

--- app/src/main/java/com/handroid/noteapp/feature_note/presentation/notes/NotesEvent.kt
+++ app/src/main/java/com/handroid/noteapp/feature_note/presentation/notes/NotesEvent.kt
@@ -1,4 +1,4 @@
-package com.handroid.noteapp.feature_note.presentation.notes.components
+package com.handroid.noteapp.feature_note.presentation.notes
 
 import com.handroid.noteapp.feature_note.domain.model.Note
 import com.handroid.noteapp.feature_note.domain.util.NoteOrder
@@ -7,5 +7,5 @@ sealed class NotesEvent {
     data class Order(val noteOrder: NoteOrder) : NotesEvent()
     data class DeleteNote(val note:Note): NotesEvent()
     object RestoreNote: NotesEvent()
-    object ToggleOrderSection:NotesEvent()
+    object ToggleOrderSection: NotesEvent()
 }
--- app/src/main/java/com/handroid/noteapp/feature_note/presentation/notes/NotesViewModel.kt
+++ app/src/main/java/com/handroid/noteapp/feature_note/presentation/notes/NotesViewModel.kt
@@ -1,4 +1,4 @@
-package com.handroid.noteapp.feature_note.presentation.notes.components
+package com.handroid.noteapp.feature_note.presentation.notes
 
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
