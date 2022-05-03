9da3c89c55875068d198c2bbf4b545735f022896
olexandr <super.lukianchuk2010@gmail.com>
2022-05-03 14:28:13 +0300

    fix hiltViewModel() from NotesScreen.kt

--- app/src/main/java/com/handroid/noteapp/feature_note/presentation/notes/NotesScreen.kt
+++ app/src/main/java/com/handroid/noteapp/feature_note/presentation/notes/NotesScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.handroid.noteapp.feature_note.presentation.notes.components.NoteItem
 import com.handroid.noteapp.feature_note.presentation.notes.components.OrderSection
@@ -24,7 +25,7 @@ import kotlinx.coroutines.launch
 @Composable
 fun NotesScreen(
     navController: NavController,
-    viewModel: NotesViewModel //TODO something miss =hiltViewModel()
+    viewModel: NotesViewModel = hiltViewModel()
 ) {
     val state = viewModel.state.value
     val scaffoldState = rememberScaffoldState()
